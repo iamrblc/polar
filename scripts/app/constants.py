ae9a3a7ddc3283d5199fb35705f2bb2a6db652fa
iamrblc <robilaci@gmail.com>
2026-06-17 11:25:08 +0200

    Constants restructured.

---
+++ scripts/app/constants.py
@@ -0,0 +1,20 @@
+POLAR = {
+    "PMDC": "fb005c81-02e7-f387-1cad-8acd2d8df0c8",
+    "PMDD": "fb005c82-02e7-f387-1cad-8acd2d8df0c8",
+    "BATTERY": "00002a19-0000-1000-8000-00805f9b34fb",
+    "ECG_FREQ": 200,
+    "ACC_FREQ": 130,
+}
+
+BELTS = {
+    "21922C2C": {
+        "name": "Polar H10 21922C2C",
+        "uuid": "8A85C5B8-B51C-277D-9490-1B3B8FFA8222",
+        "mac_address": "A0:9E:1A:21:92:2C",
+    },
+    "E6B05E24": {
+        "name": "Polar H10 E6B05E24",
+        "uuid": "FFDB0E1C-0262-9016-D154-4562DABCBE43",
+        "mac_address": "A0:9E:1A:E6:B0:5E",
+    },
+}
